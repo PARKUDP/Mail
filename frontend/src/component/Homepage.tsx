27eaa0a608b3b54ce6f9ea90c7b071de8ae4b1c0
e225752 <e225752@ie.u-ryukyu.ac.jp>
2024-08-02 19:45:57 +0900

    hoempage.tsx create

---
+++ frontend/src/component/Homepage.tsx
@@ -0,0 +1,14 @@
+import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../styles/Homepage.css';
+
+function main() {
+    return (
+        <div className='Homepage'>
+            <header className='Homepage-header'>
+                <h1>メイン画面</h1>
+            </header>
+        </div>
+    )
+}
